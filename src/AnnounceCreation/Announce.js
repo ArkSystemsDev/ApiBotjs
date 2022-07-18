f9d476772968bc81839cdc98d2c3197731146bbc
vaugusto273 <samlara273@gmail.com>
2022-07-17 23:18:29 -0300

    :sparkles: CategoriasV1
    :white_check_mark: Entra no cadastro de itens na OLX

---
+++ src/AnnounceCreation/Announce.js
@@ -0,0 +1,31 @@
+const { Console } = require('console');
+const puppeteer = require('puppeteer');
+const c = require("../AnnounceCreation/Categorias")
+require("dotenv").config();
+
+//.env
+
+require("dotenv").config();
+(async () => {
+    const browser = await puppeteer.launch({headless: false});
+    const page = await browser.newPage();
+    await page.goto('https://www.olx.com.br/');
+    console.log("[+] Acessando OLX...");
+    await page.click("[href='https://conta.olx.com.br/anuncios']");
+    console.log("[+] Logando...");
+    await page.waitForNavigation();
+    await page.type('[type="email"]', process.env.LOGIN);
+    await page.type('[type="password"]', process.env.PASSWORLD);
+    await page.waitForSelector('#cookie-notice-ok-button');
+    await page.click('#cookie-notice-ok-button');
+    const [button] = await page.$x("//button[contains(., 'Entrar')]");
+    if(button){
+        await button.click();
+        console.log("[+] Logado com sucesso!");
+    }
+    await page.waitForNavigation();
+    await page.goto('https://www2.olx.com.br/desapega');
+    await page.type('[label="Título*"]', 'teste');
+    await page.type('[label="Descrição*"]', 'teste');
+    let category_selected = c.category();
+})();
---
+++ src/AnnounceCreation/Categorias.js
@@ -0,0 +1,26 @@
+
+
+const categorias = [
+  "Imóveis",
+  "Autos e peças",
+  "Para a sua casa",
+  "Eletrônicos e celulares",
+];
+
+function ChooseCategory(param) {
+  if (param == null) {
+    console.log("NullReference");
+    return;
+  }
+}
+
+function category(_categorias_) {
+  for (let i = 0; i <= categorias.length; i++) {
+    if (_categorias_ == categorias[i]) {
+      console.log("categoria selecionada" + categorias[i]);
+      return category[i];
+    }
+  }
+}
+
+module.exports = { category };
