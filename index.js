94bc4f735e6dffe4a3dd895492e959294f19154f
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2022-07-27 11:50:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-const puppeteer = require('puppeteer');
-require("dotenv").config();
+const { GenerateAnnunces } = require("./src/products/products");
 
 
-
-(async () => {
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
-    await page.goto('https://www.olx.com.br/');
-    console.log("[+] Acessando OLX...");
-    await page.click("[href='https://conta.olx.com.br/anuncios']");
-    console.log("[+] Logando...");
-    await page.waitForNavigation();
-
-    await page.type('[type="email"]', process.env.LOGIN);
-    await page.type('[type="password"]', process.env.PASSWORD);
-
-    await page.waitForSelector('#cookie-notice-ok-button');
-
-    await page.click('#cookie-notice-ok-button');
-
-    const [button] = await page.$x("//button[contains(., 'Entrar')]");
-    if(button){
-        await button.click();
-        console.log("[+] Logado com sucesso!");
-    }
-   
-    
-})();
+async function teste(){
+    await GenerateAnnunces();
+}
