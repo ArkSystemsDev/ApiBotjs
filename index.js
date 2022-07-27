3f1496daf14d9defe4cf8fe3651aefe55d19ebb1
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2022-07-27 04:25:47 -0300

    Correção Produtos.json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const myList = require("./src/products/products.json");
 
 async function SelectProduct() {
     const data_ = JSON.parse(JSON.stringify(myList));
-    console.log(data_[0].anuncio.title);
+    console.log(data_[0].anuncio[1].description);
 
 }
 
