61c8c01d403c6dc503fa0bbb862684a453fdf587
Vitor Batista <42703631+shadomal@users.noreply.github.com>
2022-07-27 21:24:30 -0300

    Update GetPageCookies.js

--- src/cookies/GetPageCookies.js
+++ src/cookies/GetPageCookies.js
@@ -8,24 +8,35 @@ const sleep = (milliseconds) => {
 async function GetPageCookie() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+    console.log("[COOKIE_PAGE][STATUS] Opening OLX")
     await page.goto("https://www.olx.com.br/", {
         waitUntil: "networkidle2",
     });
 
-    await page.type("#__next > div > div.sc-cHSUfg.hDSJfS.sc-jTzLTM.iwtnNi > div.sc-hmXxxW.izBUKY > div.sc-iybRtq.fjyUzw.sc-jTzLTM.iwtnNi > form > div:nth-child(1) > div.sc-dEoRIm.dWonDA.sc-jTzLTM.iwtnNi > input",
-        "arktechdevelopment@gmail.com");
-    await page.click("#identifierNext");
+    try {
+        await page.click("[href='https://conta.olx.com.br/anuncios']");
+    } catch (err) {
+        console.log("[COOKIE_PAGE][ERROR] " + err);
+        await page.browser().disconnect;
+        await page.close()
 
-    await page.waitForSelector("#password", {
-        visible: true,
-        hidden: false,
-    });
-    await page.type(
-        "#__next > div > div.sc-cHSUfg.hDSJfS.sc-jTzLTM.iwtnNi > div.sc-hmXxxW.izBUKY > div.sc-iybRtq.fjyUzw.sc-jTzLTM.iwtnNi > form > div:nth-child(2) > div.sc-dEoRIm.dWonDA.sc-jTzLTM.iwtnNi > div > div > input",
-        "Sheut123"
-    );
+        //Create new page and try to get cookies
+        GetPageCookie();
+    }
+    await page.waitForNavigation();
+    await page.waitForSelector('#cookie-notice-ok-button');
+
+    console.log("[COOKIE_PAGE][+] INSERT EMAIL AND PASSWORD");
+    await page.type('[type="email"]', `${data_[index].login}`);
+    await page.type('[type="password"]', `${data_[index].password}`);
+    
     await sleep(1000);
-    await page.click("#passwordNext > div > button");
+
+    const [button] = await page.$x("//button[contains(., 'Entrar')]");
+    if (button) {
+        await button.click();
+        console.log("[+] SUCESSFULL LOGIN");
+    }
 
     await sleep(10000);
 
